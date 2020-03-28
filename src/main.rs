89cc0c4fa9ca23c7beeb722637e5ba59f4506f85
Pavlo Hlushchenko <pavlo.hlushchenko@gmail.com>
2020-03-28 19:44:11 +0200

    Implement basic HTTP proxy functionality
    This commit adds ability to use this tool as a basic HTTP proxy.
    It takes bytes from the TCP connection, forwards them to the destination
    server, reads the response and forwards it back to the client.
    It uses the value from the Host header in the request to determine the
    address of the destination server.
    Request is limited to 512B and response is limited to 64KB.
    Default bind address is 127.0.0.1:7878, but this will be configurable in
    cli argument in the future.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
+use regex::Regex;
+use std::io::{Read, Write};
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::str::from_utf8;
+
 fn main() {
-    println!("Hello, fart!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    // This is done in order not to compile the regex pattern on every call of handle_connection
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"Host: (?P<host>[0-9a-zA-Z\-\.:]+)").unwrap();
+    }
+    let caps = RE.captures(from_utf8(&buffer).unwrap()).unwrap();
+    let mut host = String::from(caps.name("host").unwrap().as_str());
+    if !host.contains(":") {
+        host.push_str(":80");
+    }
+
+    match TcpStream::connect(host) {
+        Ok(mut server_stream) => {
+            server_stream.write_all(&buffer).unwrap();
+            let mut data = [0 as u8; 64000];
+            match server_stream.read(&mut data) {
+                Ok(_) => {
+                    stream.write_all(&data).unwrap();
+                }
+                Err(e) => {
+                    println!("Failed to receive data: {}", e);
+                }
+            }
+        }
+        Err(e) => {
+            println!("Failed to connect: {}", e);
+        }
+    }
+
+    stream.flush().unwrap();
 }
